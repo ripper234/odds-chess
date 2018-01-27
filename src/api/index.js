c390087df8f48b15cebc1d5b659f3f448cc2a41c
Ron Gross <ron.gross@gmail.com>
2018-01-27 13:06:21 +0200

    Add chess.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
+import assert from 'assert';
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+
+var Chess = require('chess.js').Chess;
+var chess = new Chess();
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -13,5 +17,11 @@ export default ({ config, db }) => {
 		res.json({ version });
 	});
 
+	api.get('/getMoves', (req, res) => {
+		// game.movePiece({ from: { file: 5, rank: 2 }, to: { file: 5, rank: 4 } });
+		assert(req.query.file)
+		assert(req.query.rank)
+		res.json(chess.moves());
+	})
 	return api;
 }
